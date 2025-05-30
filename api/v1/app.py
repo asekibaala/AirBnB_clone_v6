47327a4cdd5d76c9c33254b0f5e1ba3973aaa6f7
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-05-30 04:45:01 +0300

    a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -10,6 +10,11 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """Remove current SQLAlchemy Session after each request"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """JSON 404 error handler"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
