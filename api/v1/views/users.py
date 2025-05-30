9d8b411d0137ef168f225c36fbb3455cc348340a
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-05-30 04:56:56 +0300

    view for User object that handles all default RESTFul API actions:

---
+++ api/v1/views/users.py
@@ -0,0 +1,58 @@
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object by id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a User object by id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a User object"""
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if "email" not in data:
+        return jsonify({"error": "Missing email"}), 400
+    if "password" not in data:
+        return jsonify({"error": "Missing password"}), 400
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User object by id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    ignore = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
